602543d890f2c540c87b89a4b715086214be1358
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-03-03 12:28:52 +0000

    ログイン処理を追加

--- auth_sample/views.py
+++ auth_sample/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth import views as auth_views
 
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
 
 
-class LoginView(View):
+def login(request):
     '''
     ログイン関連
     '''
-    def get(self, request):
-        
-        template_name = 'auth_sample/login.html'
-        
+    template_name = 'auth_sample/login.html'
+    
+    context = {
+        'template_name': template_name,
+        'authentication_form': LoginForm
+    }
+
+    return auth_views.login(request, **context)
+    
+    
+def logout(request):
+    '''
+    ログアウト関連
+    '''
+    template_name = 'auth_sample/index.html'
+    
+    context = {
+        'template_name': template_name,
+    }
+    
+    return auth_views.logout(request, **context)
