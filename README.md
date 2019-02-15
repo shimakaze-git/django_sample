600161944de68f162e8d8bf6483cdfe7b7054163
shimakaze-git <shimakaze.soft+github@googlemail.com>
2019-02-16 02:25:26 +0900

    feat: django tutorial

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# django_sample
+# Django-Tutorial
+
+### pollsアプリケーション
+
+
+## 参考資料
+- [はじめての Django アプリ作成](https://docs.djangoproject.com/ja/2.1/intro/)
+- [Djangoのsettings.py周りのメモ](https://qiita.com/jp_ibis/items/0ac1ba8aba1398f2a492)
+- [Djangoで、集計処理](https://narito.ninja/blog/detail/84/)
+- [モダンDjango入門](https://codezine.jp/article/detail/10722)
--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""django_sample URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls_app.polls.urls')),
     path('admin/', admin.site.urls),
-    
-    # path(r'', include('auth_sample.urls')),
 ]
